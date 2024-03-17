aef42a8d9dea7faba52776d71d17edb89aa88a04
Arber-Vasquez <arber.vasquez@unas.edu.pe>
2024-03-17 02:46:55 -0500

    test

---
+++ sesion 01/primer_programa.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // se hace uso del comando COUT para imprimir el mensaje
+    cout << "mi primer programa";
+
+    // finalizamos el programa con un return 0 para  indicar que todo ha ido bien
+    return 0;
+}
---
+++ sesion 01/saludo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    cout << "hola amigo programador";
+    cout  << "\n\tHola, como estas?";
+    // \n es un salto de linea
+    // \t es una tabulacion
+    
+    
+    return 0;
+}
